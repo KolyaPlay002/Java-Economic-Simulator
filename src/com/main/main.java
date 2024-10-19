dd6a4c9bcba090a38d372b0cc09dc5e7ea8db6a9
KolyaPlay002 <147727768+KolyaPlay002@users.noreply.github.com>
2024-10-19 22:08:21 +0300

    Add files via upload

---
+++ src/com/main/main.java
@@ -0,0 +1,26 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		String com;
+		
+		Scanner comScan = new Scanner(System.in);
+		
+		com = comScan.nextLine();
+		
+		while(true) {
+			switch(com)	{
+			case "help":
+				System.out.println("");
+				System.out.println("");
+				break;			
+			}		
+		}
+		
+	}
+
+}
